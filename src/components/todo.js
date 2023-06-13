4bf3bd752a90efc18de75c36f1acbc5f2ccdd944
Mohanaprabhu.R <mohan@timeless.co>
2023-06-13 16:07:30 +0530

    Edit functionality implement

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,13 +1,47 @@
+import React, { useState } from "react";
+
 export const Todo = ({ message, deleteTodo }) => {
-  const handleSubmit = (event) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedMessage, setEditedMessage] = useState(message);
+
+  const handleSubmit = () => {
     deleteTodo(message);
   };
+
+  const handleSave = () => {
+    deleteTodo(message);
+    deleteTodo(editedMessage);
+    setIsEditing(false);
+  };
+
   return (
     <li id="todo">
-      <span id="todo__label">{message + "  "}</span>
-      <button id="todo__delete" onClick={handleSubmit}>
-        Delete
-      </button>
+      {isEditing ? (
+        <input
+          type="text"
+          value={editedMessage}
+          onChange={(event) => setEditedMessage(event.target.value)}
+        />
+      ) : (
+        <span id="todo__label ">{message + "  "}</span>
+      )}
+      <div>
+        {isEditing ? (
+          <button className="todo_save todo__delete" onClick={handleSave}>
+            Save
+          </button>
+        ) : (
+          <button
+            className="todo_edit todo_save todo__delete "
+            onClick={() => setIsEditing(true)}
+          >
+            Edit
+          </button>
+        )}
+        <button className="todo__delete" onClick={handleSubmit}>
+          Delete
+        </button>
+      </div>
     </li>
   );
 };
--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,15 +1,19 @@
+import React from "react";
 import { Todo } from "./todo";
 
 export const TodoList = ({ messageList, deleteTodo }) => {
   return (
     <ol id="todolist">
-      {messageList &&
+      {messageList && messageList.length > 0 ? (
         messageList
           .slice(0)
           .reverse()
           .map((message, index) => (
             <Todo message={message} deleteTodo={deleteTodo} key={index} />
-          ))}
+          ))
+      ) : (
+        <li className="no-record">No records found</li>
+      )}
     </ol>
   );
 };
